6650dabccfa3d304818003b423d2561bc8a00e35
parkging <parkjun611@gmail.com>
2026-03-21 23:23:52 +0900

    Fix WebClientErrorHandler 수정;Readme 수정

--- src/main/java/site/ng_archive/ecom_common/webclient/ExternalService4xxException.java
+++ src/main/java/site/ng_archive/ecom_common/webclient/ExternalService4xxException.java
@@ -3,7 +3,13 @@ package site.ng_archive.ecom_common.webclient;
 import lombok.Getter;
 
 public class ExternalService4xxException extends RuntimeException {
-    public ExternalService4xxException(String code) {
+    @Getter
+    String _code;
+    @Getter
+    String _message;
+    public ExternalService4xxException(String code, String message) {
         super(code);
+        this._code = code;
+        this._message = message;
     }
 }
--- src/main/java/site/ng_archive/ecom_common/webclient/ExternalService5xxException.java
+++ src/main/java/site/ng_archive/ecom_common/webclient/ExternalService5xxException.java
@@ -3,7 +3,13 @@ package site.ng_archive.ecom_common.webclient;
 import lombok.Getter;
 
 public class ExternalService5xxException extends RuntimeException {
-    public ExternalService5xxException(String code) {
+    @Getter
+    String _code;
+    @Getter
+    String _message;
+    public ExternalService5xxException(String code, String message) {
         super(code);
+        this._code = code;
+        this._message = message;
     }
 }
